c087de71fd9330f9d5a5adfc06e3f3739322fb1a
Baruch Lubinsky <baruchlubinsky@gmail.com>
2012-05-17 16:50:51 +0200

    Initial commit

---
+++ matrix_me/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MatrixMe::Application.config.secret_token = '619bb1809ff62b4d060f797b4531c9711b1c122a3f5e23714e02c9379534cf877f0ea4ea542acdc6b2891c379bdcfa5efa985807e594978e94ff9fa9f41511fc'
